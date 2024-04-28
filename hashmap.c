bc21e5f1c99d6b2d7aa1d05564c249bff4b7f040
Vicente <125922459+viichorm@users.noreply.github.com>
2024-04-28 02:28:16 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -182,13 +182,21 @@ Pair * firstMap(HashMap * map) {
   }
 
   for (int i = 0; i < map->capacity; i++) {
-      if (map->buckets[i] != NULL) {
-          map->current = i;
-          return map->buckets[i];
+      if (map->buckets[i] != NULL){
+        return NULL;
+        
       }
 
-    return NULL;
-  }
+    for (int i = 0; i < map->capacity; i++){
+      if (map->buckets[i] != NULL){
+        map->current = i;
+        return map->buckets[i];
+      }
+      
+    }
+    
+  return NULL;
+}
 
 
 Pair * nextMap(HashMap * map) {
